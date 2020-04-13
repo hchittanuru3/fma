47f1380c2877c1178a54205f5e0a2395d1fee0b9
hchittanuru3 <hemanthc98@outlook.com>
2020-04-12 23:29:20 -0400

    Reformatted and have a package format

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import os
 from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='freemusicarchive',
       version='0.0.0',
@@ -6,4 +13,6 @@ setup(name='freemusicarchive',
       url='https://github.com/mdeff/fma',
       author='Michaël Defferrard',
       author_email='michael.defferrard@epfl.ch',
+      install_requires = required,
+      packages=['fma'],
       license='MIT')
